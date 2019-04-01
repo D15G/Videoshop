0dd5ea9bc8d0da7d27712bdd18d0cef0585a8f59
xXTime-OnXx <timon.schmid@bluewin.ch>
2019-04-01 09:53:25 +0200

    Ausleihe Status

--- app/controllers/StatusController.php
+++ app/controllers/StatusController.php
@@ -1,3 +1,18 @@
 <?php
 
+$ausleihen = array(
+    array (
+        'name' => 'Timon Schmid',
+        'video' => 'Otto',
+        'rueckgabe' => '20.02.2020',
+        'status' => '0'
+    ),
+    array (
+        'name' => 'David Gataric',
+        'video' => 'Baseball',
+        'rueckgabe' => '20.01.2020',
+        'status' => '1'
+    ),
+);
+
 require 'app/views/status.view.php';
--- app/views/status.view.php
+++ app/views/status.view.php
@@ -6,9 +6,34 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public/css/style.css">
     <title>Ausleihe Status</title>
 </head>
 <body>
-    <h1>Status Page</h1>
+    <div class="wrapper">
+        <h1>Status Page</h1>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Video</th>
+                <th scope="col">Rückgabe</th>
+                <th scope="col">Status</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach($ausleihen as $index => $ausleihe) { ?>
+            <tr>
+                <th scope="row"><?php echo $index ?></th>
+                <td><?php echo $ausleihe['name']; ?></td>
+                <td><?php echo $ausleihe['video']; ?></td>
+                <td><?php echo $ausleihe['rueckgabe']; ?></td>
+                <td><img class="icon" src="//github.githubassets.com/images/icons/emoji/unicode/1f601.png"></td>
+            </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
