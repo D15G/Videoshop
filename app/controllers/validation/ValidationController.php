3cb8852a2209e9662409c8cd0e00f79c1ed2b527
D15G <daavidgtr@outlook.com>
2019-04-02 12:49:47 +0200

    Formular Erfassen PHP logic

--- app/controllers/validation/ValidationController.php
+++ app/controllers/validation/ValidationController.php
@@ -1,13 +1,21 @@
 <?php
 
-function validate(string $name, string $email, string $telefon): array
+function validate(string $name, string $email, string $telefon, string $memberStatus, string $video): array
 {
 
     $errors = array();
     $regex = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/';
 
+    $movieModel = new MoviesModel();
+    $allMovies = $movieModel->getAllMovieIds();
+    $allMemberStatuses = ['', 'Bronze', 'Silber', 'Gold'];
+
     if ($name === '') {
         array_push($errors, 'Namensfeld ist leer!');
+    }
+
+    if (strlen($name) <= 4) {
+        array_push($errors, 'Der Name muss länger als 4 Zeichen sein!');
     }
 
     if ($email === '') {
@@ -18,9 +26,22 @@ function validate(string $name, string $email, string $telefon): array
         array_push($errors, 'Email ist invalid!');
     }
 
+    if (strpos($email, "google.com")) {
+        array_push($errors, 'google.com ist keine gültige Email!');
+    }
+
     if (preg_match("/[a-z]/i", $telefon)) {
         array_push($errors, 'Telefonnummer darf keine Alphabetischen Zeichen enthalten!');
     }
+
+    if (!in_array($video, $allMovies)) {
+        array_push($errors, 'Der Ausgewählte Film existiert nicht!');
+    }
+
+    if (!in_array($memberStatus, $allMemberStatuses)) {
+        array_push($errors, 'Der Ausgewählte Mitgliedschaftsstatus existiert nicht!');
+    }
+
 
 
 
